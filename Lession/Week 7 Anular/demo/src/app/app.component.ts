7b9958333462db59631f7c82b08cf0a99f2eb834
longvibol <vibollong@gmail.com>
2024-09-13 17:04:50 +0700

    erew

---
+++ Lession/Week 7 Anular/demo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { style } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls:['./app.component.css'],
+  template: '<h2>Hello Angular!</h2>',
+  
+})
+export class AppComponent {
+  title = 'demo';
+}
