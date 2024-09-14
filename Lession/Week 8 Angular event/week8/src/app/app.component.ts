97f9d7ba22de0c0c0c43512849ead590982a2e8b
Your Namem <vibollong@gmail.com>
2024-09-14 17:03:01 +0700

    'er

--- Lession/Week 8 Angular event/week8/src/app/app.component.ts
+++ Lession/Week 8 Angular event/week8/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'week8';
+
+  name:string="";
+
+  showMessage(){
+    // alert(this.name);
+
+    this.name ="Thida"
+
+  }
+
 }
