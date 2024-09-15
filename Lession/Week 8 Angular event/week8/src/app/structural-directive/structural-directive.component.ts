2308383d5c4d678c3b745c482775914f7d29dd47
vibollong <vibollong@gmail.com>
2024-09-15 19:47:50 +0700

    wer

--- Lession/Week 8 Angular event/week8/src/app/structural-directive/structural-directive.component.ts
+++ Lession/Week 8 Angular event/week8/src/app/structural-directive/structural-directive.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructuralDirectiveComponent implements OnInit {
 
+  color ="yellow";
+
+  colors =["red","blue","yellow","black"];
+
 
   isCambodia = false;
 
