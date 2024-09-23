45954e9e3f83b8f7ecd2bebf1d80b532254aefab
longvibol <vibollong@gmail.com>
2024-09-23 14:37:00 +0700

    finished

---
+++ Lession/Week 12 Tracking State and Validity/angular-form/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { Student } from './model/student';
+import { inject } from '@angular/core/testing';
+import { StudentService } from './model/student.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+  subjects = ["Java","PHP","C#","Pyton"];
+  student = new Student("Dara","dara@gmail.com","Java","Male"); 
+
+  isValidSubject = false;
+  isSubmitted = false;
+
+
+  selectSubject(subject: string){
+    if(subject === ""){
+      this.isValidSubject = false;
+    }else{
+      this.isValidSubject =true;
+    }
+  }
+
+  constructor(private studentService: StudentService){}
+
+  doSubmit(){
+    this.isSubmitted =true;
+    console.log("submit work")
+    this.studentService.saveStudent(this.student);
+  }
+
+
+}
